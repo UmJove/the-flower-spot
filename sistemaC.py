c4711b2d2167a9aca51ab116122d7c70f297962d
Luan B Almada <lba.letras@gmail.com>
2025-07-23 10:37:22 -0300

    Adicionando confirmação de pedido

--- sistemaC.py
+++ sistemaC.py
@@ -10,7 +10,6 @@ produtos = {
 
 carrinho = {}
 dados_do_pedido = {}
-
 def listar_produtos():
     print("\nProdutos disponíveis:")
     for id, info in produtos.items():
@@ -67,8 +66,21 @@ def finalizar_compra():
     ver_carrinho()
     if carrinho:
         formulario_compra() # função de preenchimento de formulário para finalização do pedido
-        print("\nCompra finalizada! Obrigado pela preferência.")
-        carrinho.clear()
+        pedido_confirmado = confirmar_pedido() # ver dados de formulário + carrinho com total
+        
+        if pedido_confirmado == True:
+            print("\n "\
+            "\n Compra finalizada! Obrigado pela preferência.")
+            carrinho.clear()
+        
+        elif pedido_confirmado == "menu":
+            menu()
+        
+        elif pedido_confirmado == False:
+            print("PEDIDO CANCELADO")
+            sair()
+
+        
     else:
         print("Seu carrinho está vazio.")
 
@@ -76,14 +88,17 @@ def formulario_compra(): # incluir verificações de valores (try/except)
         print("\nPara finalizar sua compra forneça as informações a seguir: \n")
         comprador = input("Nome do comprador: ")
         cpf = input("CPF: ") 
+        email = input("E-mail: ")
         endereco_rua = input("Endereço(logradouro): ")
         endereco_numero = input("Endereço(numero): ")
+        endereco_complemento = input("Endereço (complemento): ")
         forma_pagamento = input("Escolha uma forma de pagamento:\n 1 - Crédito\n 2 - Débito\n 3 - Pix\n ")
+        entrega = input("Escolha uma forma de entrega:\n 1 - Retirar na loja\n 2 - Receber em casa\n ")
         
         dados_do_pedido["comprador"] = comprador
         dados_do_pedido["cpf"] = cpf
-        dados_do_pedido["endereço"] = {endereco_rua:endereco_numero}
-        dados_do_pedido["pedido"] = carrinho
+        dados_do_pedido["e-mail"] = email
+        dados_do_pedido["endereço"] = [endereco_rua, endereco_numero, f"complemento: {endereco_complemento}"]
         if forma_pagamento == "1":
             dados_do_pedido["forma de pagamento"] = "crédito"
         elif forma_pagamento == "2":
@@ -94,17 +109,43 @@ def formulario_compra(): # incluir verificações de valores (try/except)
         # else:
         #     raise ValueError ("opção inválida, escolha novamente") 
         
-        print("\n>> Dados do pedido <<")
-        for c,v in dados_do_pedido.items():
-            print(f"{c} - {v}")
-            
+        if entrega == "1":
+            dados_do_pedido["entrega"] = "retirar na loja"
+        elif entrega == "2":
+            dados_do_pedido["entrega"] = "receber em casa"
+
+def confirmar_pedido():
+    print("\n Confirme as informações a seguir para finalizar seu pedido"
+    "\n >> Dados do pedido <<")
+    for c,v in dados_do_pedido.items():
+        print(f"{c} - {v}")
+    ver_carrinho()
+    confirmacao = input("\nConfirmar pedido(S/N): "
+    "\n sim - confirmar"
+    "\n não - cancelar pedido e encerrrar programa"
+    "\n voltar - voltar ao menu\n ")
+    if confirmacao.lower() == "sim":
+        return True
+    elif confirmacao.lower() == "não" or confirmacao.lower() == "nao":
+        return False
+    elif confirmacao.lower() == "voltar":
+        return "menu"
+    else: #aprimorar
+        print("Resposta não identificada, tente novamente!")
+        confirmar_pedido()
+
+
+
+def sair():
+    print("Encerrando o sistema. Até logo!")
+    exit()
 
 def menu():
     while True:
         print("\n--- Menu ---")
         print("1. Listar produtos")
         print("2. Adicionar ao carrinho")
-        print("3. Remover do carrinho") #SE TIVER VAZIO SAIR DA ROTINA
+        print("3. Remover do carrinho") #SE TIVER VAZIO SAIR DA ROTINA (Já alterado)
         print("4. Ver carrinho")
         print("5. Finalizar compra")
         print("6. Sair")
@@ -122,7 +163,7 @@ def menu():
         elif opcao == "5":
             finalizar_compra()
         elif opcao == "6":
-            print("Encerrando o sistema. Até logo!")
+            sair()
             break
         else:
             print("Opção inválida. Tente novamente.")
